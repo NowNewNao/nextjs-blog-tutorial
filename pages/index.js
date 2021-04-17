3c0faa203d0bb45b5a15c14b0081dd1dd087aeee
nao <nownewnao777@gmail.com>
2021-04-17 23:56:08 +0900

    GET blog data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -10,6 +20,20 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi! I'm Nao. I'm a software engineer! You can connect me on <a href="https://twitter.com/NowNewNao" target="_blank">Twitter</a> !</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
